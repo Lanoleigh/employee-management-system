d55b65e486216acc2f3e5c7affe28589c02543f1
Zalano <zalano16p@gmail.com>
2025-11-23 23:24:57 +0200

    Initial Commit

---
+++ src/app/component/header/header.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+})
+export class Header {
+  constructor(private router: Router) {}
+
+  goToLogin(path: string): void {
+    this.router.navigate(['/login']);
+  }
+}
